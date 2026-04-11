d146a261b149129ab3bdff5f66fe431bf2a3842c
Quiet Orchestra <quiet.orchestrator@gmail.com>
2026-04-11 13:17:50 -0700

    Restart Wasm web branch

---
+++ wasm/wasm_build_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rustup target add wasm32-unknown-unknown
+
+export RUSTFLAGS='--cfg=web_sys_unstable_apis --cfg getrandom_backend="wasm_js"'
+.
+cargo clean
+cargo build --features webgl --release --target wasm32-unknown-unknown || exit 1
+wasm-bindgen --no-typescript --target web \
+    --out-dir ./wasm/ \
+    --out-name "prismatic_visualizer" \
+    ./target/wasm32-unknown-unknown/release/prismatic_visualizer.wasm || exit 1
+
+wasm-opt -O -ol 100 -s 100 -o wasm/prismatic_visualizer_bg.wasm wasm/prismatic_visualizer_bg.wasm || exit 1
+
+echo "Build and optimization completed successfully."
